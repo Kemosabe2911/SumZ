0f2959a19855b06762dc841b5cadf26f5d266f66
Kemosabe2911 <stevinprince29@gmail.com>
2023-04-23 20:15:05 +0530

    rapid api call complete

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 
 import { copy, linkIcon, loader, tick, enter } from '../assets'
+import { useLazyGetSummaryQuery } from '../services/article'
 
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -8,8 +9,19 @@ const Demo = () => {
     summary: '',
   });
 
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
+
   const handleSubmit = async (e) => {
-    alert('Submitted')
+    e.preventDefault()
+    const { data } = await getSummary({ articleUrl: article.url})
+
+    if(data?.summary) {
+      const newArticle = { ...article, summary: data.summary}
+
+      setArticle(newArticle)
+
+      console.log(newArticle)
+    }
   }
 
   return (
