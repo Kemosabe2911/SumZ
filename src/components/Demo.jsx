578217ddef12b9fabc9604d14b57762c51f80a6d
Kemosabe2911 <stevinprince29@gmail.com>
2023-04-23 21:26:14 +0530

    copy to clipboard

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,6 +9,7 @@ const Demo = () => {
     summary: '',
   });
   const [allArticles, setAllArticles] = useState([])
+  const [copied, setCopied] = useState("")
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
 
@@ -33,6 +34,12 @@ const Demo = () => {
 
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
     }
+  }
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl)
+    navigator.clipboard.writeText(copyUrl)
+    setTimeout(() => setCopied(false), 3000)
   }
 
   return (
@@ -77,9 +84,9 @@ const Demo = () => {
               onClick={() => setArticle(item)}
               className='link_card'
             >
-              <div className='copy_btn'>
+              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
                 <img 
-                  src={copy}
+                  src={copied === item.url ? tick : copy}
                   alt='copy_icon'
                   className='w-[40%] h-[40%] object-contain'
                 />
